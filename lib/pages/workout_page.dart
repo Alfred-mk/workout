31474a926f97f4fc887b6637b9b4e5863d93cc51
alfred-mk <alfredmwaik@gmail.com>
2023-02-21 10:31:02 +0300

    create a new exercise and add local storage

--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -18,11 +18,106 @@ class _WorkoutPageState extends State<WorkoutPage> {
         .checkOffExercise(workoutName, exerciseName);
   }
 
+  // text controllers
+  final exerciseNameController = TextEditingController();
+  final exerciseWeightController = TextEditingController();
+  final exerciseRepsController = TextEditingController();
+  final exerciseSetsController = TextEditingController();
+
+  // create a new exercise
+  void createNewExercise() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add a new exercise'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // exercise name
+            TextField(
+              controller: exerciseNameController,
+            ),
+
+            // weight
+            TextField(
+              controller: exerciseWeightController,
+            ),
+
+            // reps
+            TextField(
+              controller: exerciseRepsController,
+            ),
+
+            // sets
+            TextField(
+              controller: exerciseSetsController,
+            ),
+          ],
+        ),
+        actions: [
+          // save button
+          MaterialButton(
+            onPressed: save,
+            child: Text("save"),
+          ),
+
+          // cancel button
+          MaterialButton(
+            onPressed: cancel,
+            child: Text("cancel"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // save workout
+  void save() {
+    // get data from text controllers
+    String exerciseName = exerciseNameController.text;
+    String exerciseWeight = exerciseWeightController.text;
+    String exerciseReps = exerciseRepsController.text;
+    String exerciseSets = exerciseSetsController.text;
+
+    // add exercise to workout
+    Provider.of<WorkoutData>(context, listen: false).addExercise(
+      widget.workoutName,
+      exerciseName,
+      exerciseWeight,
+      exerciseReps,
+      exerciseSets,
+    );
+
+    // pop dialog box
+    Navigator.pop(context);
+
+    clear();
+  }
+
+  // cancel
+  void cancel() {
+    // pop dialog box
+    Navigator.pop(context);
+    clear();
+  }
+
+  // clear controllers
+  void clear() {
+    exerciseNameController.clear();
+    exerciseWeightController.clear();
+    exerciseRepsController.clear();
+    exerciseSetsController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<WorkoutData>(
       builder: (context, value, child) => Scaffold(
         appBar: AppBar(title: Text(widget.workoutName)),
+        floatingActionButton: FloatingActionButton(
+          onPressed: createNewExercise,
+          child: Icon(Icons.add),
+        ),
         body: ListView.builder(
           itemCount: value.numberOdExercisesInWorkout(widget.workoutName),
           itemBuilder: (context, index) => ExerciseTitle(

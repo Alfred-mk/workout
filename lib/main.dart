4348818cb0263d026dfe2cd348cbf5fa5df5e889
alfred-mk <alfredmwaik@gmail.com>
2023-02-21 15:30:57 +0300

    store and retrieve records from a hive database

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:workout/data/workout_data.dart';
 import 'pages/home_page.dart';
 
-void main() {
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+
+  // open a hive box
+  await Hive.openBox("workout_database");
+
   runApp(const MyApp());
 }
 
